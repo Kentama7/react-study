6efa99514fec4d7227d80561c0c140fcdcd50f58
Kentama7 <kentama.7m@gmail.com>
2021-02-14 09:46:18 +0900

    Add filter

--- moz-todo-react/src/App.js
+++ moz-todo-react/src/App.js
@@ -4,28 +4,33 @@ import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import {nanoid} from "nanoid";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: task => !task.completed,
+  Completed: task => task.completed
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
 
-  function addTask(name) {
-    const newTask = {id: "todo-" + nanoid(), name: name, completed: false};
-    setTasks([...tasks, newTask])
-  }
+  const [filter, setFilter] = useState('All');
 
   const [tasks, setTasks] = useState(props.tasks);
 
-  const taskList = tasks.map(task => (
-    <Todo
-      id={task.id}
-      name={task.name}
-      completed={task.completed}
-      key={task.id}
-      toggleTaskCompleted={toggleCompleted}
-      deleteTask={deleteTask}
-    />
-  ));
-
-  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
-  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+  const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map(task => (
+      <Todo
+        id={task.id}
+        name={task.name}
+        completed={task.completed}
+        key={task.id}
+        toggleTaskCompleted={toggleCompleted}
+        deleteTask={deleteTask}
+        editTask={editTask}
+      />
+    ));
 
   function toggleCompleted(id) {
     const updatedTasks = tasks.map(task => {
@@ -37,17 +42,45 @@ function App(props) {
     setTasks(updatedTasks);
   }
 
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
+  function addTask(name) {
+    const newTask = {id: "todo-" + nanoid(), name: name, completed: false};
+    setTasks([...tasks, newTask])
+  }
+
   function deleteTask(id) {
     const remainingTasks = tasks.filter(task => id !== task.id);
     setTasks(remainingTasks);
   }
+
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map(task => {
+      if (id === task.id) {
+        return {...task, name: newName}
+      }
+      return task;
+    });
+    setTasks(editedTaskList)
+  }
+
+  const filterList = FILTER_NAMES.map(name => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));
 
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
       <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
-        <FilterButton/>
+        {filterList}
       </div>
       <h2 id="list-heading">{headingText}</h2>
       <ul
